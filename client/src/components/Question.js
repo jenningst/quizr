79817109f56a31ea18913881817dca57f5da5288
Troy Jennings <troy.c.jennings@gmail.com>
2019-04-13 16:33:03 +0200

    Quiz now controls question

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ButtonBase from './common/base/ButtonBase';
@@ -7,12 +7,17 @@ import AnswerItem from './AnswerItem';
 const ERROR_MESSAGE = "Whoops! Try again!";
 const SUCCESS_MESSAGE = "That's correct!";
 
-const Question = ({ question, getNextQuestion, isCorrect }) => {  
+const Question = ({ question, submitResponses, isCorrect, getNextQuestion, attemptsLeft }) => {  
   const [selectedChoices, setSelectedChoices] = useState([]);
-  const [assistMode, setAssistMode] = useState(false);
-  const [mode, setMode] = useState(false);
-  // const [isCorrect, setIsCorrect] = useState(false);
   const [message, setMessage] = useState("");
+  
+  useEffect(() => {
+    setSelectedChoices([]);
+  }, [attemptsLeft]);
+
+  useEffect(() => {
+    setSelectedChoices([]);
+  }, [isCorrect]);
 
   return (
     <QuestionWrapper className="question-wrapper">
@@ -30,7 +35,22 @@ const Question = ({ question, getNextQuestion, isCorrect }) => {
           />
         ))}
         <ActionWrapper className="action-buttons">
-          {renderSubmitOrNext()}
+          {isCorrect
+            ? <ActionButton 
+                backgroundColor="#6121bf"
+                borderColor="#4717a2"
+                type="button" 
+                onClick={fetchNextQuestion}
+              >
+                Next Question
+              </ActionButton>
+            : <ActionButton
+                type="button" 
+                onClick={sendChoicePayload}
+              >
+                Submit
+              </ActionButton>
+          }
         </ActionWrapper>
       </AnswerList>
       <MessageBox className="message-box">
@@ -39,38 +59,13 @@ const Question = ({ question, getNextQuestion, isCorrect }) => {
     </QuestionWrapper>
   );
 
-  function renderSubmitOrNext() {
-    // Dynamically renders a button; renders a "submit" button if at least one
-    // choice has been selected and renders a "next question" button if the 
-    // question has been answered correctly.
-    if (selectedChoices.length > 0) {
-      if (isCorrect === true) { //render next question button
-        return (
-          <ActionButton 
-            backgroundColor="#6121bf"
-            borderColor="#4717a2"
-            type="button" 
-            onClick={fetchQuestion}
-          >
-            Next Question
-          </ActionButton>
-        );
-      } else { // render submit current question button
-        return (
-          <ActionButton
-            type="button" 
-            onClick={checkAnswer}
-          >
-            Submit
-          </ActionButton>
-        );
-      }
-    }
-    return null;
+  // Submits an array of selected choices for validation
+  function sendChoicePayload() {
+    submitResponses(selectedChoices);
   }
 
+  // Adds or removes a element from the selectedChoices state
   function selectChoice(index) {
-    // Adds or removes a choice selection and updates local state
     let selections = [...selectedChoices];
     if (!selections.includes(index)) {
       selections.push(index);
@@ -83,88 +78,10 @@ const Question = ({ question, getNextQuestion, isCorrect }) => {
     setSelectedChoices(selections);
   }
 
-  function checkAnswer() {
-    // Checks the validity of an answer set; has two modes: (a)
-    // quick check and assist mode
-    let choices = selectedChoices; // choices = [3, 0]
-    const ANSWERS = getAnswers(); // answers = [2]
-
-    function onError() {
-      // setIsCorrect(false);
-      setMessage(ERROR_MESSAGE);
-      setSelectedChoices([]);
-    }
-
-    function onSuccess() {
-      // setIsCorrect(true);
-      setMessage(SUCCESS_MESSAGE);
-    }
-
-    if (assistMode === false) { // REFACTOR: Change this to reflect test mode
-
-      if (choices.length !== ANSWERS.length) { // check for length equality
-        onError();
-        return;
-      } else { // same number of answers
-        if (ANSWERS.every(answer => choices.includes(answer))) {
-          onSuccess();
-          return;
-        }
-        onError();
-        return;
-      }
-    } else { // assist mode enabled; provide output the user
-      //let correct, incorrect, missing;
-      // loop through the answers:
-      // if answers.includes(THE VALUE, NOT INDEX) === true:
-      //   push the correct answer to correct
-      //   remove the correct answer from choices
-      // else:
-      //   push the missing answer to missing
-      // finally:
-      //   anything left over in choices gets concatenated into incorrect
-  
-      // invalid cases:
-      // partial incorrect (missing one or more correct)
-      //    answer = [1, 2, 3]; choices = [1, 2]
-      //    display incorrect to user
-      //    show them correct answers [1, 2]
-      //    show them incorrect answers [3]
-      // partial incorrect (extraneous selection)
-      //    answer = [1, 3]; choices = [1, 2, 3]
-      //    show them correct answers [1, 3]
-      //    show them incorrect answers [2]
-      // all incorrect
-      //    answer = [2, 4]; choices = [0, 1, 3]
-      //    show them correct answers []
-      //    show them incorrect answers [0, 1, 3]
-      onError();
-      return;
-    }
-  }
-
-  function fetchQuestion() {
-    getNextQuestion();
-    // re-initialize state
+  // Sets a request to Quiz for the next question
+  function fetchNextQuestion() {
     setSelectedChoices([]);
-    // setIsCorrect(false);
-    setMessage("");
-  }
-
-  function toggleAssistMode() {
-    // Toggles assist mode for the question
-    setAssistMode(!assistMode);
-  }
-
-  function getAnswers() {
-    // Returns an array of correct choice indexes
-    let answerIndexes = [];
-    question.choices.map((choice, index) => {
-        if (choice.isAnswer === true) {
-          answerIndexes.push(index);
-        }
-      });
-    return answerIndexes;
+    getNextQuestion();
   }
 };
 
--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -9,9 +9,12 @@ const Quiz = () => {
   // state for all modes
   const [questions, setQuestions] = useState([]);
   const [index, setIndex] = useState(0);
+  const [message, setMessage] = useState("");
   // state for intensive mode
   const [mode, setMode] = useState(MODES.INTENSIVE_MODE);
+  const [correct, setCorrect] = useState(false);
   const [correctCount, setCorrectCount] = useState(0);
+  const [attemptCounter, setAttemptCounter] = useState(3);
 
   useEffect(() => {
     // pre-load questions
@@ -20,19 +23,95 @@ const Quiz = () => {
 
   return (
     <QuizWrapper>
+      <h1>You have {attemptCounter} attemps left</h1>
       {index && index === questions.length
         ? <Assessment
           />
-        : <Question
-              question={SAMPLE_QUESTIONS[index]}
-              getNextQuestion={getNextQuestion}
-          />
+        : attemptCounter > 0
+            ?  <Question
+                  question={SAMPLE_QUESTIONS[index]}
+                  getNextQuestion={getNextQuestion}
+                  isCorrect={correct}
+                  submitResponses={checkAnswer}
+                  attemptsLeft={attemptCounter}
+              />
+            : <h1>No attemps left!</h1>
       }
     </QuizWrapper>
   );
 
+  function checkAnswer(choicePayload) {
+    // Checks the validity of a choices payload from Question
+    // reduce attempts
+    setAttemptCounter(attemptCounter - 1);
+
+    let choices = choicePayload;
+    const ANSWERS = getAnswers();
+
+    const onError = () => {
+      // setMessage(ERROR_MESSAGE);
+      setCorrect(false);
+      
+    }
+
+    const onSuccess = () => {
+      // setMessage(SUCCESS_MESSAGE);
+      setCorrect(true);
+      setAttemptCounter(3);
+    }
+    
+    if (choices.length !== ANSWERS.length) { // check for length equality
+      onError();
+      return;
+    } else { // same number of answers
+      if (ANSWERS.every(answer => choices.includes(answer))) {
+        onSuccess();
+        return;
+      }
+      onError();
+      return;
+    }
+    
+    //let correct, incorrect, missing;
+    // loop through the answers:
+    // if answers.includes(THE VALUE, NOT INDEX) === true:
+    //   push the correct answer to correct
+    //   remove the correct answer from choices
+    // else:
+    //   push the missing answer to missing
+    // finally:
+    //   anything left over in choices gets concatenated into incorrect
+
+    // invalid cases:
+    // partial incorrect (missing one or more correct)
+    //    answer = [1, 2, 3]; choices = [1, 2]
+    //    display incorrect to user
+    //    show them correct answers [1, 2]
+    //    show them incorrect answers [3]
+    // partial incorrect (extraneous selection)
+    //    answer = [1, 3]; choices = [1, 2, 3]
+    //    show them correct answers [1, 3]
+    //    show them incorrect answers [2]
+    // all incorrect
+    //    answer = [2, 4]; choices = [0, 1, 3]
+    //    show them correct answers []
+    //    show them incorrect answers [0, 1, 3]
+  }
+
+  function getAnswers() {
+    // Helper method; Returns an array of correct choice indexes
+    let answerIndexes = [];
+    SAMPLE_QUESTIONS[index].choices.map((choice, index) => {
+      if (choice.isAnswer === true) {
+        answerIndexes.push(index);
+      }
+    });
+    return answerIndexes;
+  }
+
   function getNextQuestion() {
     setIndex(index + 1);
+    setCorrect(false);
   }
 };
 
@@ -41,3 +120,11 @@ const QuizWrapper = styled.div`
 `;
 
 export default Quiz;
+
+// function fetchQuestion() {
+//   getNextQuestion();
+//   // re-initialize state
+//   setSelectedChoices([]);
+//   // setIsCorrect(false);
+//   setMessage("");
+// }
